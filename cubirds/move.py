eb557043c3a559b12d8be70f101ae97e32397d22
Nuno Silva <NunoMCSilva@gmail.com>
2022-01-08 15:02:00 +0000

    Add __repr__ to Side

--- cubirds/move.py
+++ cubirds/move.py
@@ -2,12 +2,18 @@ from __future__ import annotations
 
 from abc import ABC
 from dataclasses import dataclass
-from enum import Enum
+from enum import Enum, auto
 
 from bird import Bird   # TODO: not really necessary, but PyCharm keeps warning me about it's "lack"
 
 
-Side = Enum('Side', 'LEFT RIGHT')
+# Side = Enum('Side', 'LEFT RIGHT')
+class Side(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+
+    def __repr__(self):
+        return self.name.capitalize()
 
 
 class Move(ABC):
